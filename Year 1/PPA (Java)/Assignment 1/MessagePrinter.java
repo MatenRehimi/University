f5d3c7cc2fc1b6697ed477d81153a36d31479fec
MatenRehimi <matenrehimi@hotmail.com>
2021-07-15 19:01:16 +0100

    "Add assignment 1"

---
+++ Year 1/PPA (Java)/Assignment 1/MessagePrinter.java
@@ -0,0 +1,8 @@
+public class MessagePrinter {
+    
+    public static void main(String[] args) {
+        Messages messages = new Messages();
+        messages.wordPrinter();
+        messages.incrementPrinter(4);
+    }
+}
---
+++ Year 1/PPA (Java)/Assignment 1/Messages.java
@@ -0,0 +1,15 @@
+public class Messages {
+
+    public void namePrinter() {
+        System.out.println("Maten Rehimi");
+    }
+
+    public void wordPrinter() {
+        System.out.println("Hotel");
+    }
+
+    public int incrementPrinter(int n) {
+        return n+1;
+    }
+    
+}
